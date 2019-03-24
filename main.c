805cdc106ea402e4fea38267a6177f603f4e3dfd
TrovatoCarla <47337438+TrovatoCarla@users.noreply.github.com>
2019-03-24 19:13:34 -0300

    masMinimoMaximo

--- main.c
+++ main.c
@@ -14,14 +14,35 @@ float PedirNumerosySacarPromedio ()
     int acumulador=0;
     float promedio;
     int i;
+    int maximo;
+    int minimo;
 
     for(i=0;i<5;i++)
     {
         printf("Ingrese un numero: ");
         scanf("%d",&numeroIngresado);
+        if(i==0)
+        {
+            maximo=numeroIngresado;
+            minimo=numeroIngresado;
+        }
+        else
+        {
+            if(numeroIngresado>maximo)
+            {
+                maximo=numeroIngresado;
+            }
+            if(numeroIngresado<minimo)
+            {
+                minimo=numeroIngresado;
+            }
+        }
+
         acumulador=acumulador+numeroIngresado;
     }
     promedio=(float)acumulador/i;
     printf("\nEl promedio es: %.2f",promedio);
-    return promedio;
+    printf("\nEl numero minimo es:%d",minimo);
+    printf("\nEl numero maximo es:%d",maximo);
+    return 0;
 }
