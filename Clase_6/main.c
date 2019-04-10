0c66ca79d1cf7ae6e928fd3f35064b4e210dba33
alumno <alumno@LAB00PC00>
2019-04-09 21:38:14 -0300

    Ejercicio de clase 6

--- Clase_6/main.c
+++ Clase_6/main.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>// en esta biblioteca esta strncpy
+#define MAX 20
 
 int main()
 {
-    char nombre[257]={'H','O','L','A','\0'};//ESPACIO PARA 256,DECLARO UNO MAS POR EL /0. asi inicializo
-    char nombre2[257]="HOLA";// asi inicializo
+    char buffer[MAX];
+    char nombre[MAX];
+    int i;
+    printf("\nIngrese nombre: ");
+    fgets(buffer,sizeof(nombre),stdin);
 
-    //NOOOOOOOOOO nombre="Juan";
+    strncpy(nombre,buffer,sizeof(nombre));
 
-    strncpy(nombre,nombre2,sizeof(nombre)/*257*/);//le doy el tamaño del origen-asi no la quiere ver escrita la funcion
-    fgets(nombre,sizeof(nombre),stdin);//va a leer un string desde un archivo,y dejo de usar scanf
-    printf("\n%s",nombre);
+    //i=strlen(buffer);
+    //printf("\n %d",i-1);
+
+    for(i=0;i<MAX && buffer[i]!='\0';i++)
+    {
+
+        if((buffer[i]<'a' || buffer[i]>'z') && (buffer[i]<'A' || buffer[i]>'Z'))
+          {
+                printf("Error!!!");
+                break;
+           }
+    }
 
     return 0;
 }
+
+
+
+  //  char nombre[257]={'H','O','L','A','\0'};//ESPACIO PARA 256,DECLARO UNO MAS POR EL /0. asi inicializo
+  //  char nombre2[257]="HOLA";// asi inicializo
+
+    //NOOOOOOOOOO nombre="Juan";
+
+ //   strncpy(nombre,nombre2,sizeof(nombre)/*257*/);//le doy el tamaño del origen-asi no la quiere ver escrita la funcion
+  //  printf("\n%s",nombre);
+ //   fgets(nombre,sizeof(nombre),stdin);//va a leer un string desde un archivo,y dejo de usar scanf-sezeof(nombre) hace que
+                                        // si escribe mas cantidad de carateres que lo permitido no pase nada,solo toma la
+                                        //cantidad permitida
+    //NOOO scanf("%s",nombre)
+ //   printf("\n%s",nombre);
+ //   strlen()
+
+ //   return 0;
