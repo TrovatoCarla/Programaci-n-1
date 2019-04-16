7ff738f013d73569b3555d31cc7cfb39a76bf78d
alumno <alumno@LAB00PC00>
2019-04-16 19:30:04 -0300

    EStr

--- Clase7_Estructuras/main.c
+++ Clase7_Estructuras/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcionesUTN.h"
+#include "misValid.h"
+
 int main()
 {
-    printf("Hello world!\n");
+    char auxName[500] = "test";
+
+    char arrayNombres[QTY_EMPLEADOS][50];
+
+    strncpy(arrayNombres[0],"JUAN",50);
+
+
+    if(getName( "Nombre?: ",
+                "Error",
+                2,
+                5,
+                2,
+                auxName) == 0)
+    {
+        printf("OK: %s",auxName);
+    }
+    else
+    {
+        printf("ER: %s",auxName);
+    }
     return 0;
 }
